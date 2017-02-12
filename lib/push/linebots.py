f0f4f4e6273409e0e4406cec37f523bee836a2a1
challenging <rongqichen@rongqis-iMac.Home>
2017-02-12 11:43:23 +0800

    [Fix] - bug fixing

--- lib/push/linebots.py
+++ lib/push/linebots.py
@@ -47,8 +47,7 @@ def booking_tra_ticket(driver="phantom"):
 
             message = TemplateSendMessage(alt_text=txt_not_support(), template=ConfirmTemplate(text=txt, actions=[
                     MessageTemplateAction(label="取消訂票", text='ticket_tra=cancel+{}'.format(ticket_number)),
-                    MessageTemplateAction(label="繼續訂票", text='ticket_tra=again'),
-                    MessageTemplateAction(label="切換模式", text='切換模式')
+                    MessageTemplateAction(label="繼續訂票", text='ticket_tra=again')
                 ]))
 
             line_bot_api.push_message(user_id, message)
@@ -75,8 +74,7 @@ def booking_thsr_ticket(driver="phantom"):
 
             message = TemplateSendMessage(alt_text=txt_not_support(), template=ConfirmTemplate(text=txt, actions=[
                     MessageTemplateAction(label="取消訂票", text='ticket_thsr=cancel+{}'.format(ticket_number)),
-                    MessageTemplateAction(label="繼續訂票", text='ticket_thsr=again'),
-                    MessageTemplateAction(label="切換模式", text='切換模式')
+                    MessageTemplateAction(label="繼續訂票", text='ticket_thsr=again')
                 ]))
 
             line_bot_api.push_message(user_id, message)
