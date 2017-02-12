8b28f314e287b05251d038e08ae05b97920919e3
rongqichen <javawokr@gmail.com>
2017-02-12 09:01:27 +0800

    [Code] - remove something which we don't need

--- app/push.py
+++ app/push.py
@@ -11,13 +11,8 @@ from lib.push.linebots import booking_tra_ticket, booking_thsr_ticket
 @click.command()
 def run():
     while True:
-        log("start to process cracker tasks of TRA")
         booking_tra_ticket()
-        log("end the cracker tasks of TRA")
-
-        log("start to process cracker tasks of THSR")
         booking_thsr_ticket()
-        log("end the cracker tasks of THSR")
 
         time.sleep(random.randint(15, 30))
 
