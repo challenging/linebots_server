05124056a3805086eccba8177600b29f7517f582
rongqichen <javawokr@gmail.com>
2017-02-14 14:33:56 +0800

    [Code] - enrich the debug message

--- lib/bot/bus.py
+++ lib/bot/bus.py
@@ -177,9 +177,9 @@ class BusBot(Bot):
                 #    adjusted_timestamp = (crawling_timestamp - now)/60
 
                 if estimation > 0:
-                    #print direction.encode(UTF8), now, crawling_timestamp, adjusted_timestamp, estimation,
+                    print direction.encode(UTF8), now, crawling_timestamp, adjusted_timestamp, estimation,
                     estimation -= adjusted_timestamp
-                    #print estimation
+                    print estimation
                     estimation = round(estimation, 1)
 
                     direction = direction.encode(UTF8)
