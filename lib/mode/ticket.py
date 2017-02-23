f9d838028f41ce9ef9688278582414572c1dc809
rongqichen <javawokr@gmail.com>
2017-02-23 21:01:22 +0800

    [Fix] - bug fixing

--- lib/mode/ticket.py
+++ lib/mode/ticket.py
@@ -439,6 +439,7 @@ class TicketMode(Mode):
             body = body.strip()
             number = ticket[u"票號"]
             messages.append(MessageTemplateAction(label=txt_ticket_failed(), text='ticket_{}={}+{}'.format(ticket_type, TICKET_STATUS_FAILED, number)))
+            messages.append(MessageTemplateAction(label=txt_ticket_memory(), text='ticket_{}={}+{}'.format(type, TICKET_STATUS_MEMORY, number)))
         else:
             log("Not found this ticket type - {}".format(ticket_type))
 
@@ -470,8 +471,7 @@ class TicketMode(Mode):
             messages = []
             for ticket in tickets:
                 number, body, m = self.get_ticket_body(ticket, ticket_type, status, headers)
-                message = [MessageTemplateAction(label=text_cancel_label, text='ticket_{}={}+{}'.format(ticket_type, text_cancel_text, number)),
-                           MessageTemplateAction(label=txt_ticket_memory(), text='ticket_{}={}+{}'.format(type, TICKET_STATUS_MEMORY, number))]
+                message = [MessageTemplateAction(label=text_cancel_label, text='ticket_{}={}+{}'.format(ticket_type, text_cancel_text, number))]
                 message.extend(m)
 
                 messages.append(CarouselColumn(text=body, actions=message))
