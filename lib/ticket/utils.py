baa838b77473dab56eeaca704254e7495d8b46b9
rongqichen <javawokr@gmail.com>
2017-03-17 16:56:54 +0800

    [Fix] - bug fixing

--- lib/ticket/utils.py
+++ lib/ticket/utils.py
@@ -7,7 +7,7 @@ import requests
 import urllib
 import urllib2
 
-from lib.common.utils import check_folder, data_dir, get_phantom_driver, get_chrome_driver
+from lib.common.utils import check_folder, data_dir
 
 TICKET_RETRY = 4
 TICKET_COUNT = 4
@@ -286,10 +286,6 @@ def thsr_ticket_dir():
 def thsr_cancel_dir():
     return thsr_dir("cancel")
 
-opener = None
-if opener is None:
-    opener = get_chrome_driver()
-
 class TRAUtils(object):
     TRA_CANCELED_URL = "http://railway.hinet.net/ccancel_rt.jsp"
     TRA_QUERY_URL = "http://railway.hinet.net/coquery.jsp"
@@ -299,15 +295,27 @@ class TRAUtils(object):
         global opener
 
         url = "{}?personId={}&orderCode={}".format(TRAUtils.TRA_CANCELED_URL, person_id.upper(), ticket_number)
+        print url
 
-        '''
+        headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                   "Accept-Encoding": "gzip, deflate, sdch",
+                   "Accept-Language": "zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4",
+                   "Connection": "keep-alive",
+                   "Host": "railway.hinet.net",
+                   "Referer": "http://railway.hinet.net/ccancel_rt.jsp?personId={}&orderCode={}".format(person_id.upper(), ticket_number),
+                   "Upgrade-Insecure-Requests": "1",
+                   "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
+        print headers
+
         request = urllib2.Request(url, headers=headers)
         f = urllib2.urlopen(request)
         content = unicode(f.read(), f.headers.getparam('charset'))
-        '''
+        print content
 
+        '''
         opener.get(url)
         content = opener.find_element_by_xpath("//p[@class=\"orange02\"]").text
+        '''
 
         is_passing = False
         if content == u"您的車票取消成功":
@@ -343,4 +351,4 @@ if __name__ == "__main__":
         print k["Train"]
     '''
 
-    print TRAUtils.is_canceled("l122760167", "151846")
+    print TRAUtils.is_canceled("l122760167", "537391")
